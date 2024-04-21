c8c4ad8fd384e3dd8e4d805bf075cf741f2a281d
ME-001 <assassincreea002@gmail.com>
2024-04-21 12:06:40 +0530

    s = 5.02TeV hBeamEnergy = 2510 eBeamEnergy = 2510

--- SartreNoon/Rev/Afterburner.C
+++ SartreNoon/Rev/Afterburner.C
@@ -61,6 +61,15 @@ void Afterburner()
     std::vector<Double_t> onXn;
     std::vector<Double_t> XnXn;
 
+    std::vector<Double_t> ononEta;
+    std::vector<Double_t> onXnEta;
+    std::vector<Double_t> XnXnEta;
+
+    std::vector<Int_t> on;
+    std::vector<Int_t> ln;
+    std::vector<Int_t> Xn;
+
+
     Double_t Mv = 3.09;
 
     for(Long64_t index = 0; index < nE; index++)
@@ -69,16 +78,32 @@ void Afterburner()
 
         Double_t y = lv->Rapidity();
 
+        Double_t eta= lv->Eta();
+
         Double_t k = 0.5*Mv*TMath::Exp(TMath::Abs(y));
 
         std::vector<Int_t> nNumbers = gen->runSartreNoon(k);
 
-        if(nNumbers[0]==0 & nNumbers[1] == 0) onon.push_back(y);
-        else if(nNumbers[0]==0 || nNumbers[1]==0 )onXn.push_back(y);
-        else if(nNumbers[0]!=0 & nNumbers[1]!=0) XnXn.push_back(y);
+        if(nNumbers[0]==0 & nNumbers[1] == 0) 
+        {
+            onon.push_back(y);
+            ononEta.push_back(eta);
+            on.push_back(index);
+        }
+        else if(nNumbers[0]==0 || nNumbers[1]==0 )
+        {
+            onXn.push_back(y);
+            onXnEta.push_back(eta);
+            ln.push_back(index);
+        }
+        else if(nNumbers[0]!=0 & nNumbers[1]!=0) 
+        {
+            XnXn.push_back(y);
+            XnXnEta.push_back(eta);
+            Xn.push_back(index);
+        }
         else std::cout<<"Error in the Event"<<std::endl;
     }
-
     delete lv;
     delete tree;
     file->Close();
@@ -89,41 +114,95 @@ void Afterburner()
     std::cout<<"XnXn number of events: "<<XnXn.size()<<std::endl;
 
     // Create histograms for each rapidity list
-    TH1F *hist1 = new TH1F("hist1", "Rapidity Distribution 1", 100, -5.0, 5.0);
-    TH1F *hist2 = new TH1F("hist2", "Rapidity Distribution 2", 100, -5.0, 5.0);
-    TH1F *hist3 = new TH1F("hist3", "Rapidity Distribution 3", 100, -5.0, 5.0);
+    TH1F *hist1 = new TH1F("hist1", "Rapidity Distribution 1", 1000, -10.0, 10.0);
+    TH1F *hist2 = new TH1F("hist2", "Rapidity Distribution 2", 1000, -10.0, 10.0);
+    TH1F *hist3 = new TH1F("hist3", "Rapidity Distribution 3", 1000, -10.0, 10.0);
+
+    TH1F *Eta1 = new TH1F("Eta1", "PseudoRapidity Distribution 1", 1000, -10.0, 10.0);
+    TH1F *Eta2 = new TH1F("Eta2", "PseudoRapidity Distribution 2", 1000, -10.0, 10.0);
+    TH1F *Eta3 = new TH1F("Eta3", "PseudoRapidity Distribution 3", 1000, -10.0, 10.0);
+
+    TH1F *E1 = new TH1F("E1", "PseudoRapidity Distribution 1", 1000, -10.0, 10.0);
+    TH1F *E2 = new TH1F("E2", "PseudoRapidity Distribution 2", 1000, -10.0, 10.0);
+    TH1F *E3 = new TH1F("E3", "PseudoRapidity Distribution 3", 1000, -10.0, 10.0);
+
+    // TLorentzVector *lv;
+
+    // TTree *tre = dynamic_cast<TTree*>(file->Get("tree"));
+
+    // TLorentzVector* l = new TLorentzVector();
+
+    // TBranch *b = tre->GetBranch("vm");
+
+    // bc->SetAddress(&lv);
 
     // Fill histograms with rapidity values
-    for (double value : onon) {
-        hist1->Fill(value);
+    for (Int_t i; i<onon.size();i++) {
+        
+        // bc->GetEntry(on[i]);
+
+        hist1->Fill(onon[i]);
+        Eta1->Fill(ononEta[i]);
+        // E1->Fill(lv->E());
+
     }
-    for (double value : onXn) {
-        hist2->Fill(value);
+    for (Int_t i; i<onXn.size();i++) {
+        
+        // bc->GetEntry(ln[i]);
+        
+        hist2->Fill(onXn[i]);
+        Eta2->Fill(onXn[i]);
+        // E2->Fill(lv->E());
     }
-    for (double value : XnXn) {
-        hist3->Fill(value);
+    // for (Int_t i; i<onXn.size();i++) {
+        
+    //     // bc->GetEntry(ln[i]);
+        
+    //     // hist2->Fill(onXn[i]);
+    //     Eta2->SetBinContent(onXn[i],0.5*Eta2->GetBinContent(onXn[i]));
+    //     // E2->Fill(lv->E());
+    // }
+    for (Int_t i; i<XnXn.size();i++) {
+
+        // bc->GetEntry(Xn[i]);
+
+        hist3->Fill(XnXn[i]);
+        Eta3->Fill(XnXnEta[i]);
+        // E3->Fill(lv->E());
     }
 
     // Create histograms for cross-section distribution
-    TH1F *XS1 = new TH1F("XS1", "Xsection Distribution 1", 100, -3.5, 4.0);
-    TH1F *XS2 = new TH1F("XS2", "Xsection Distribution 2", 100, -3.5, 4.0);
-    TH1F *XS3 = new TH1F("XS3", "Xsection Distribution 3", 100, -3.5, 4.0);
+    TH1F *XS1 = new TH1F("XS1", "Xsection Distribution 1", 1000, -10.0, 10.0);
+    TH1F *XS2 = new TH1F("XS2", "Xsection Distribution 2", 1000, -10.0, 10.0);
+    TH1F *XS3 = new TH1F("XS3", "Xsection Distribution 3", 1000, -10.0, 10.0);
+    TH1F *XST = new TH1F("XST", "Total Cross section", 1000, -10.0, 10.0);
 
-    // Fill cross-section histograms
-    for (double value : onon) {
-        Int_t bin = hist1->FindBin(value);
-        Double_t crossSection = hist1->GetBinContent(bin) * 524/100000;
-        XS1->Fill(value, crossSection);
-    }
-    for (double value : onXn) {
-        Int_t bin = hist2->FindBin(value);
-        Double_t crossSection = hist2->GetBinContent(bin) * 524/100000;
-        XS2->Fill(value, crossSection);
-    }
-    for (double value : XnXn) {
-        Int_t bin = hist3->FindBin(value);
-        Double_t crossSection = hist3->GetBinContent(bin) * 524/100000;
-        XS3->Fill(value, crossSection);
+    // // Fill cross-section histograms
+    // for (double value : onon) {
+    //     Int_t bin = hist1->FindBin(value);
+    //     Double_t crossSection = hist1->GetBinContent(bin);
+    //     XS1->Fill(value, crossSection);
+    // }
+    // for (double value : onXn) {
+    //     Int_t bin = hist2->FindBin(value);
+    //     Double_t crossSection = hist2->GetBinContent(bin) ;
+    //     XS2->Fill(value, crossSection);
+    // }
+    // for (double value : XnXn) {
+    //     Int_t bin = hist3->FindBin(value);
+    //     Double_t crossSection = hist3->GetBinContent(bin) ;
+    //     XS3->Fill(value, crossSection);
+    // }
+    // XS1 = hist1;
+    // XS2 = hist2;
+    // XS3 = hist3;
+
+    for(Int_t entry = 0; entry<1000; entry++)
+    {
+        XS1->SetBinContent(entry,hist1->GetBinContent(entry) * 524 * 1000 /100000);
+        XS2->SetBinContent(entry,hist2->GetBinContent(entry) * 524 * 1000 /100000);
+        XS3->SetBinContent(entry,hist3->GetBinContent(entry) * 524 * 1000 /100000);
+        XST->SetBinContent(entry,XS1->GetBinContent(entry)+XS2->GetBinContent(entry)+XS3->GetBinContent(entry));
     }
 
     // Create a canvas to draw histograms
@@ -133,24 +212,103 @@ void Afterburner()
     XS1->SetLineColor(kRed);
     XS2->SetLineColor(kBlue);
     XS3->SetLineColor(kGreen);
+    XST->SetLineColor(kBlack);
 
     // Draw histograms
-    XS1->Draw("");
-    XS2->Draw("same");
-    XS3->Draw("same");
+    XS1->GetXaxis()->SetRangeUser(-4,4);
+    XS1->GetYaxis()->SetRangeUser(0,5000);
+    XS1->Draw("L");
+    XS2->GetXaxis()->SetRangeUser(-4,4);
+    XS2->GetYaxis()->SetRangeUser(0,5000);
+    XS2->Draw("SAME L");
+    XS3->GetXaxis()->SetRangeUser(-4,4);
+    XS3->GetYaxis()->SetRangeUser(0,5000);
+    XS3->Draw("SAME L");
+    XST->GetXaxis()->SetRangeUser(-4,4);
+    XST->GetYaxis()->SetRangeUser(0,5000);
+    XST->Draw("Same L");
 
     // Add legend
     TLegend *legend = new TLegend(0.7, 0.7, 0.9, 0.9);
     legend->AddEntry(XS1, "0n0n", "l");
     legend->AddEntry(XS2, "0nXn", "l");
     legend->AddEntry(XS3, "XnXn", "l");
+    legend->AddEntry(XST, "Total", "l");
     legend->Draw();
 
     // Draw canvas
     c1->Draw();
     c1->SaveAs("Classes_of_cross_section.png");
 
+    
+    // delete legend;
+    
 
+    TCanvas *c2 = new TCanvas("c2", "Pseudo Rapidity", 1200, 800);
 
+    
+    Eta1->SetLineColor(kRed);
+    Eta2->SetLineColor(kBlue);
+    Eta3->SetLineColor(kGreen);
+
+    // Draw histograms
+    Eta1->GetYaxis()->SetRangeUser(0, 500);
+    Eta1->Draw("L");
+    Eta2->GetYaxis()->SetRangeUser(0, 500);
+    Eta2->Draw("SAME L");
+    Eta3->GetYaxis()->SetRangeUser(0, 500);
+    Eta3->Draw("SAME L");
+
+    // // Add legend
+    // TLegend *legend = new TLegend(0.7, 0.7, 0.9, 0.9);
+    // legend->AddEntry(Eta1, "0n0n", "l");
+    // legend->AddEntry(Eta2, "0nXn", "l");
+    // legend->AddEntry(Eta3, "XnXn", "l");
+    // legend->Draw();
+
+    c2->Draw();
+    c2->SaveAs("Pseudo_rapidity.png");
+
+    // std::cout<<"Pseudo Rapidity of 0n0n: "<<ononEta<<std::endl;
+    // std::cout<<"Pseudo Rapidity of 0n0n: "<<onXnEta<<std::endl;
+    // std::cout<<"Pseudo Rapidity of 0n0n: "<<XnXnEta<<std::endl;
+
+    std::ofstream outFile("pseudo_rapidity.txt");
+
+    if (!outFile.is_open()) {
+        std::cerr << "Error opening the file." << std::endl;
+        return;
+    }
+
+    outFile << "Pseudo Rapidity of 0n0n:\tPseudo Rapidity of 0nXn:\tPseudo Rapidity of XnXn:" << std::endl;
+    
+    size_t maxSize = std::max({ononEta.size(), onXnEta.size(), XnXnEta.size()});
+
+    for (size_t i = 0; i < maxSize; ++i) {
+        // Output values from ononEta
+        if (i < ononEta.size()) {
+            outFile << ononEta[i] << "\t";
+        } else {
+            outFile << "\t"; // Empty column if vector is shorter
+        }
+
+        // Output values from onXnEta
+        if (i < onXnEta.size()) {
+            outFile << onXnEta[i] << "\t";
+        } else {
+            outFile << "\t"; // Empty column if vector is shorter
+        }
+
+        // Output values from XnXnEta
+        if (i < XnXnEta.size()) {
+            outFile << XnXnEta[i];
+        }
+
+        outFile << std::endl; // Move to the next line after each set of values
+    }
+
+    outFile.close();
+
+    std::cout << "Data written to pseudo_rapidity.txt" << std::endl;
 
 }
