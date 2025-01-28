082f069c62d9dfff7dd031d62a7b5578a71f73df
ME-001 <assassincreea002@gmail.com>
2025-01-28 18:07:50 +0530

    Cleaning sartreMain.cpp script

--- sartreb/gemini/CMakeFiles/gemini.dir/CWeight.cpp.o.d
+++
@@ -1,4 +0,0 @@
-gemini/CMakeFiles/gemini.dir/CWeight.cpp.o: \
- /home/bhakta/Sartre-Noon/sartre/gemini/CWeight.cpp \
- /usr/include/stdc-predef.h \
- /home/bhakta/Sartre-Noon/sartre/gemini/CWeight.h
--- sartrei/sartre/examples/CMakeFiles/convergenceTest.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for convergenceTest.
-# This may be replaced when dependencies are built.
